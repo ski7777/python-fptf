f4f01c423bf420e942e14e11206314aced8a27b0
Raphael Jacob <r.jacob2002@gmail.com>
2018-04-14 20:08:06 +0200

    AttributeBadTypeException support multiple types

--- fptf/AttributeBadTypeException.py
+++ fptf/AttributeBadTypeException.py
@@ -1,8 +1,13 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
 #
+import inspect
 
 
 class AttributeBadTypeException(Exception):
     def __init__(self, attribute, valtype):
-        super().__init__("Attribute \"" + attribute + "\" must be type " + valtype.__name__ + "!")
+        if isinstance(valtype, list) and not inspect.isclass(valtype):
+            name = ", ".join([v.__name__ for v in valtype])
+        else:
+            name = valtype.__name__
+        super().__init__("Attribute \"" + attribute + "\" must be type " + name + "!")
